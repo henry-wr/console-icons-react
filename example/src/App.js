9552ba137cebfaa7cccd846c4f44ce85f653bf64
henry-wr <henrywahyu@gmail.com>
2020-04-01 01:07:02 +1000

    cleanup files in src

--- example/src/App.js
+++ example/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'console-icons-react'
-import {FancyHeading, AccountBox, AccountCircle} from 'console-icons-react'
-import {Iconz} from 'console-icons-react'
+import {AccountBox, AccountCircle} from 'console-icons-react'
 
 export default class App extends Component {
   sayHello() {
@@ -12,11 +10,8 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
-        <FancyHeading>This is a fancy heading <AccountCircle className="small-icon" /></FancyHeading>
         <AccountBox className="small-icon" onClick={this.sayHello}/>
         <AccountCircle className="small-icon"/>
-        <Iconz.AccountCircle />
       </div>
     )
   }
