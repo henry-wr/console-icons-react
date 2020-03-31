56b57b15bc7c4cd964092e981a8ed91e1c37a52a
henry-wr <henrywahyu@gmail.com>
2020-04-01 01:02:51 +1000

    modified input path for rollup build to src/js/components/index.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ import svgr from '@svgr/rollup'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/js/components/index.js',
   output: [
     {
       file: pkg.main,
